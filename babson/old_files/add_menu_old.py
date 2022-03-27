e75a2ecac5d461d24d682414294e5d938a3e13e5
pen1064 <penelope.kwong@mail.utoronto.ca>
2022-03-26 23:44:22 -0400

    change menu parsing method
    master menu created with the recipe number there for debugging the reasons why some days we don't have options

--- babson/old_files/add_menu_old.py
+++ babson/old_files/add_menu_old.py
@@ -5,17 +5,16 @@ import json
 from dateutil.parser import parse
 
 OUT_FILE_PATH = 'menu_items.json'
-MENU_PRIOR_UPDATED = 'master_menu/menu_backup.csv'
-MENU_LATEST = 'master_menu/menu.csv'
-
 SCHOOL_ID = 10
 MLTYPE_CONST1 = ['Breakfast', 'Lunch', 'Dinner']
+
 
 def clean_table(dfc):
     for key in dfc.keys():
         if dfc[key].isna().all() == True:
             dfc.drop(key, axis=1, inplace=True)
     return dfc
+
 
 def meal(z):
     if "Breakfast" in z:
@@ -33,7 +32,7 @@ def meal(z):
         return "4:30 pm", "dinner"
 
 
-def weekly_menu_dict(dfm, dfd, row_begin, row_end):
+def weekly_menu_dict(dfm, dfd, row_begin, row_end, pk_begin):
     if row_begin == 0:
         dfmw = dfm.loc[row_begin:row_end - 1, :].reset_index(drop=True)
         week_num = int(str(list(dfmw.keys())[-1]).strip('Week'))
@@ -74,20 +73,23 @@ def weekly_menu_dict(dfm, dfd, row_begin, row_end):
 
     DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     days_d = dict(zip(DAYS, list(dfmw.keys())))
-    
-    meals, timestamps, groups, names, weeknums, meals_recipe_num = [[] for i in range(6)]
+
     days_d['Week'] = week_num
 
+    MENU_WEEKLY = []
+    pk_val = pk_begin
     for d in DAYS:
         for meal_key, df_t in dfmw.groupby('Meal'):
+            food = {'model': 'backend.MealSelection', 'pk': str(pk_val)}
+            fields = {}
             f = []
             df_t = df_t.reset_index(drop=True)
+            fields['group'] = meal_key
             s = re.sub(r"[\([{})\]]", '', days_d[d].strip('d')) + ' ' + df_t.loc[0, 'Meal_Time']
             tt = parse(s)
-            timestamps.append(tt.strftime("%Y-%m-%d %H:%M:%SZ"))
-            groups.append(meal_key)
-            names.append(meal_key.title() + " on " + d + ', ' + tt.strftime("%B %d, %Y"))
-            weeknums.append(week_num)
+            fields['timestamp'] = tt.strftime("%Y-%m-%d %H:%M:%SZ")
+            fields['name'] = meal_key.title() + " on " + d + ', ' + tt.strftime("%B %d, %Y")
+            fields['school'] = SCHOOL_ID
             for index, row in df_t.iterrows():
                 try:
                     dish = float(row[days_d[d]])
@@ -97,42 +99,20 @@ def weekly_menu_dict(dfm, dfd, row_begin, row_end):
             f = [x for x in f if str(x) != 'nan']
             q = [int(dfd.loc[dfd['cafeteria_id'] == x, 'pk'].values[0])
                  for x in f if not dfd.loc[dfd['cafeteria_id'] == x, 'pk'].empty]
-            meals.append(q)
-            meals_recipe_num.append(f)
-   
-    MENU_WEEKLY = pd.DataFrame()
-    MENU_WEEKLY['group'] = groups
-    MENU_WEEKLY['timestamp'] = timestamps
-    MENU_WEEKLY['name'] = names
-    MENU_WEEKLY['items'] = meals
-    MENU_WEEKLY['week_num'] = weeknums
-    MENU_WEEKLY['items_recipe_num'] = meals_recipe_num
-    
-    MENU_WEEKLY['items'] = MENU_WEEKLY['items'].apply(lambda x: list(map(int, x)))
+
+            fields['items'] = q
+            food['fields'] = fields
+            MENU_WEEKLY.append(food)
+            pk_val += 1
     return MENU_WEEKLY, dfmw
 
-def parse_fixture(d, out_filename):
-    results = d.to_dict(orient='index')
-    json_list=[]
-    for r in range(len(results)):
-        data = {'model': 'backend.MealSelection', 'pk': {}, 'fields': {}}
-        f = results[r]
-        data['pk'] = f['pk']
-        for e in ['pk', 'week_num','items_recipe_num']:
-            f.pop(e)
-        f['school'] = SCHOOL_ID
-        data['fields'] = f
-        json_list.append(data)
-
-    with open(out_filename, 'w') as out_file:
-        json.dump(json_list, out_file, sort_keys=False)
-    out_file.close()    
 
 def main():
+    #dfd = pd.read_csv(r'Nutrition_Table_W9-11_2022.csv', converters={'cafeteria_id': lambda x: str(x)})
     dfd = pd.read_csv(r'master_nutrition\nutrition_table.csv', converters={'cafeteria_id': lambda x: str(x)})
-    #dfm = pd.read_excel(r'menu\MenuWorks_W9-12_2022.xlsx', skiprows=11)
-    dfm = pd.read_excel(r'menu\MenuWorks_W11-13_2022.xlsx', skiprows=11)
-
+    #dfm = pd.read_excel(r'menu\MenuWorks_Week_at_a_0AA22C06-D72C-48B4-9498-F6FBC87E079F.xlsx', skiprows=11)
+    dfm = pd.read_excel(r'menu\MenuWorks_W11_2022.xlsx', skiprows=11)
+   
     dfm = clean_table(dfm)
 
     k = str(list(dfm.keys())[0])
@@ -141,27 +121,16 @@ def main():
         if 'TRIM' in str(dfm.loc[int(i), k]):
             split_row.append(i)
 
-    MENU = pd.DataFrame()
-    try:
-        df_master = pd.read_csv(MENU_LATEST)
-        df_master.to_csv(MENU_PRIOR_UPDATED,index=False) #save a backup of one version older
-        df_master.drop(['pk'], axis=1, inplace=True)
-        df_master['items'] = df_master['items'].apply(lambda x: x.tolist())
-    except:
-        df_master = pd.DataFrame()
+    MENU = []
+    pk_begin = 1000
+    for mm in range(len(split_row) - 1):
+        menuw1, dfmw1 = weekly_menu_dict(dfm, dfd, split_row[mm], split_row[mm + 1], pk_begin)
+        pk_begin = pk_begin + len(menuw1) - 1
+        MENU.extend(menuw1)
+    with open(OUT_FILE_PATH, 'w') as out_file:
+        json.dump(MENU, out_file, sort_keys=False)
+    out_file.close()
 
-    for mm in range(len(split_row)):
-        if mm == (len(split_row)-1):
-            menuw1, dfmw1 = weekly_menu_dict(dfm, dfd, split_row[mm], len(dfm))
-        else:
-            menuw1, dfmw1 = weekly_menu_dict(dfm, dfd, split_row[mm], split_row[mm + 1])
-        MENU = pd.concat([MENU, menuw1]).reset_index(drop=True)
-        
-    df_master = pd.concat([df_master, MENU]).reset_index(drop=True)
-    df_master.drop_duplicates(subset=['name'], keep='last',inplace=True, ignore_index=True)
-    df_master ['pk'] = range(1000, 1000 + len(df_master))
-    df_master.to_csv(MENU_LATEST, index=False)
-    parse_fixture(df_master , OUT_FILE_PATH)    
 
 if __name__ == '__main__':
     main()
